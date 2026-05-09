8bcf1d3a47ca6ccfa2e875217b2bcffa975f9ead
Sanjarbek <sanjik0510@gmail.com>
2026-05-09 23:58:09 +0300

    Add tests and CI/CD workflows

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-
 from src.main import app
 
 client = TestClient(app)
@@ -26,29 +25,10 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
-
-def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
-    pass
-
-def test_create_user_with_invalid_email():
-    '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
-
-def test_delete_user():
-    '''Удаление пользователя'''
-    pass
-
-
-def test_get_unexisted_user():
-    '''Получение несуществующего пользователя'''
     response = client.get("/api/v1/user", params={'email': 'nonexistent@mail.com'})
     assert response.status_code == 404
     assert response.json() == {"detail": "User not found"}
-    
-    
-    
+
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
     new_user = {
@@ -63,21 +43,17 @@ def test_create_user_with_valid_email():
     get_response = client.get("/api/v1/user", params={'email': new_user['email']})
     assert get_response.status_code == 200
     assert get_response.json()['name'] == new_user['name']
-    
-    
-    
-    def test_create_user_with_invalid_email():
-    #Создание пользователя с почтой, которую использует другой пользователь"
-     existing_user = {
+
+def test_create_user_with_invalid_email():
+    '''Создание пользователя с почтой, которую использует другой пользователь'''
+    existing_user = {
         'name': 'Duplicate User',
         'email': users[0]['email']  # используем email существующего пользователя
     }
     response = client.post("/api/v1/user", json=existing_user)
     assert response.status_code == 409
     assert response.json() == {"detail": "User with this email already exists"}
-    
-    
-    
+
 def test_delete_user():
     '''Удаление пользователя'''
     # Сначала создаем нового пользователя
@@ -99,7 +75,3 @@ def test_delete_user():
     # Проверяем, что пользователь удален
     get_after_delete = client.get("/api/v1/user", params={'email': new_user['email']})
     assert get_after_delete.status_code == 404
-    
-    
-    
-    
